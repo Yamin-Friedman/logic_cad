b502418d7472253cfbcc091e3a0542959035137d
Yamin <yaminrf@gmail.com>
2018-11-09 15:06:09 +0200

    Finished part 3 functions. Output is correct for one test but still need to check correctness more thoroughly.

--- main.cpp
+++ main.cpp
@@ -13,14 +13,28 @@
 using namespace std;
 
 int get_depth_node(hcmNode *curr_node){
-
+	int depth = 1;
+	int curr_inst_port_depth;
+	int max_inst_port_depth = 0;
+	map<string,hcmInstPort*>::const_iterator nIP;
+	if(curr_node->getInstPorts().empty())
+		return 0;
+	for(nIP = curr_node->getInstPorts().begin();nIP != curr_node->getInstPorts().end();nIP++){
+		hcmPort *inst_port_port = (*nIP).second->getPort();
+		hcmNode *inst_port_node = inst_port_port->owner();
+		curr_inst_port_depth = get_depth_node(inst_port_node);
+		if(curr_inst_port_depth > max_inst_port_depth)
+			max_inst_port_depth = curr_inst_port_depth;
+	}
+	depth += max_inst_port_depth;
+	return depth;
 }
 
-int get_max_depth(hcmCell *top_node){
+int get_max_depth(hcmCell *top_cell){
 	map<string,hcmNode*>::const_iterator nI;
 	int curr_depth = 0;
 	int max_depth = 0;
-	for(nI = top_node->getNodes().begin();nI != top_node->getNodes().end(); nI++){
+	for(nI = top_cell->getNodes().begin();nI != top_cell->getNodes().end(); nI++){
 		curr_depth = get_depth_node((*nI).second);
 		if (curr_depth > max_depth)
 			max_depth = curr_depth;
@@ -80,8 +94,9 @@ int main(int argc, char **argv) {
 
 	int num_nodes = top_cell->getNodes().size();
 
-	//int max_depth = get_max_depth(top_cell);
+	int max_depth = get_max_depth(top_cell);
 
 	cout << "instance count = " << num_instances << endl;
 	cout << "Node count = " << num_nodes << endl;
+	cout << "Max depth = " << max_depth << endl;
 }
