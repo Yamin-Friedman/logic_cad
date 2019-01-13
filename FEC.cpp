f51d30ba811edd01a39ffd88c29b4922740a75e6
Yamin <yaminrf@gmail.com>
2019-01-13 21:28:28 +0200

    Added a function to parse the input.

--- FEC.cpp
+++ FEC.cpp
@@ -17,37 +17,47 @@ using namespace std;
 bool verbose = false;
 
 
-//Clause functions:
+void parse_input(int argc, char **argv, vector<string> *spec_vlgFiles, vector<string> *implementation_vlgFiles,
+                 string *spec_top_cell_name, string *implementation_top_cell_name) {
+	vector<string> *vlgFiles_ptr = NULL;
+	int any_err = 0;
 
-
-
-int main(int argc, char **argv) {
-	int anyErr = 0;
-	unsigned int i;
-	vector<string> vlgFiles;
-	string top_cell_name;
-
-
-	// Parse input
-	if (argc < 4) {
-		anyErr++;
+	if (argc < 7) {
+		any_err++;
 	} else {
-		top_cell_name = argv[1];
-
-		for (int argIdx = 4; argIdx < argc; argIdx++) {
-			vlgFiles.push_back(argv[argIdx]);
-		}
-
-		if (vlgFiles.size() < 1) {
-			cerr << "-E- At least top-level and single verilog file required for spec model" << endl;
-			anyErr++;
+		for (int i = 1; i < argc; i++) {
+			if (argv[i] == "-v") {
+				verbose = true;
+			} else if (argv[i] == "-s") {
+				vlgFiles_ptr = spec_vlgFiles;
+				*spec_top_cell_name = argv[i + 1];
+				i++;
+			} else if (argv[i] == "-i") {
+				vlgFiles_ptr = implementation_vlgFiles;
+				*implementation_top_cell_name = argv[i + 1];
+				i++;
+			} else {
+				vlgFiles_ptr->push_back(argv[i]);
+			}
 		}
 	}
 
-	if (anyErr) {
-		cerr << "Usage: " << argv[0] << "  [-v] top-cell file1.v [file2.v] ... \n";
+	if (any_err > 0) {
+		cerr << "Usage: " << argv[0] << "   [-v] -s <spec-cell > <verilog-1> [<verilog-2> … ] -i <implemenattion-cell > <verilog-1> [<verilog-2> … ]" << endl;
 		exit(1);
 	}
+}
+
+int main(int argc, char **argv) {
+	unsigned int i;
+	vector<string> spec_vlgFiles;
+	vector<string> implementation_vlgFiles;
+	string spec_top_cell_name;
+	string implementation_top_cell_name;
+
+
+	parse_input(argc,argv,&spec_vlgFiles,&implementation_vlgFiles,&spec_top_cell_name,&implementation_top_cell_name);
+
 
 	// Build HCM design
 	set<string> globalNodes;
