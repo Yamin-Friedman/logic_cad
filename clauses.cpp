6af30e2249ea051dad862e525feebeaa1e8a742e
OmerKepten <44835031+omerkepten@users.noreply.github.com>
2019-01-18 12:33:25 +0200

    finished adding FFs to PO,PI maps. created xnor2 clause but have yet to implement in FEC.cpp (and push to SAT)
    (cherry picked from commit ba5b31b)
    
    This is my best attempt to merge your patch

--- clauses.cpp
+++ clauses.cpp
@@ -2,36 +2,10 @@
 
 using namespace std;
 
-vector<vector<int>> buffer_clause(int input_var, int output_var) {
-	vector<vector<int>> clauses;
-
-	if (input_var == 0) {
-		clauses.push_back(vector<int>(1,0));
-	} else if (input_var == -1) {
-		clauses.push_back(vector<int>(1,-1));
-	} else {
-		vector<int> pos_clause{input_var, -output_var};
-		vector<int> neg_clause{-input_var, output_var};
-		clauses.push_back(pos_clause);
-		clauses.push_back(neg_clause);
-	}
-
-	return clauses;
-}
-
 vector<vector<int>> not_clause(int input_var, int output_var) {
-	vector<vector<int>> clauses;
-
-	if (input_var == 0) {
-		clauses.push_back(vector<int>(1,-1));
-	} else if (input_var == -1) {
-		clauses.push_back(vector<int>(1,0));
-	} else {
-		vector<int> pos_clause{input_var, output_var};
-		vector<int> neg_clause{-input_var, -output_var};
-		clauses.push_back(pos_clause);
-		clauses.push_back(neg_clause);
-	}
+	vector<int> pos_clause{input_var,output_var};
+	vector<int> neg_clause{-input_var,-output_var};
+	vector<vector<int>> clauses{pos_clause,neg_clause};
 
 	return clauses;
 }
@@ -41,15 +15,9 @@ vector<vector<int>> and_clause(vector<int> input_var, int output_var) {
 	vector<int> sum_clause{output_var};
 
 	for (int i = 0; i < input_var.size(); i++) {
-		if (input_var[i] == 0) {
-			return vector<vector<int>>(1,vector<int>(1,0));
-		} else if (input_var[i] == -1) {
-			continue;
-		} else {
-			sum_clause.push_back(-input_var[i]);
-			vector<int> clause{-output_var, input_var[i]};
-			clauses.push_back(clause);
-		}
+		sum_clause.push_back(-input_var[i]);
+		vector<int> clause{-output_var,input_var[i]};
+		clauses.push_back(clause);
 	}
 
 	clauses.push_back(sum_clause);
@@ -62,15 +30,9 @@ vector<vector<int>> nand_clause(vector<int> input_var, int output_var) {
 	vector<int> sum_clause{-output_var};
 
 	for (int i = 0; i < input_var.size(); i++) {
-		if (input_var[i] == 0) {
-			return vector<vector<int>>(1,vector<int>(1,-1));
-		} else if (input_var[i] == -1) {
-			continue;
-		} else {
-			sum_clause.push_back(-input_var[i]);
-			vector<int> clause{output_var, input_var[i]};
-			clauses.push_back(clause);
-		}
+		sum_clause.push_back(-input_var[i]);
+		vector<int> clause{output_var,input_var[i]};
+		clauses.push_back(clause);
 	}
 
 	clauses.push_back(sum_clause);
@@ -83,15 +45,9 @@ vector<vector<int>> or_clause(vector<int> input_var, int output_var) {
 	vector<int> sum_clause{-output_var};
 
 	for (int i = 0; i < input_var.size(); i++) {
-		if (input_var[i] == 0) {
-			continue;
-		} else if (input_var[i] == -1) {
-			return vector<vector<int>>(1,vector<int>(1,-1));
-		} else {
-			sum_clause.push_back(input_var[i]);
-			vector<int> clause{output_var, -input_var[i]};
-			clauses.push_back(clause);
-		}
+		sum_clause.push_back(input_var[i]);
+		vector<int> clause{output_var,-input_var[i]};
+		clauses.push_back(clause);
 	}
 
 	clauses.push_back(sum_clause);
@@ -104,15 +60,9 @@ vector<vector<int>> nor_clause(vector<int> input_var, int output_var) {
 	vector<int> sum_clause{output_var};
 
 	for (int i = 0; i < input_var.size(); i++) {
-		if (input_var[i] == 0) {
-			continue;
-		} else if (input_var[i] == -1) {
-			return vector<vector<int>>(1,vector<int>(1,0));
-		} else {
-			sum_clause.push_back(input_var[i]);
-			vector<int> clause{-output_var, -input_var[i]};
-			clauses.push_back(clause);
-		}
+		sum_clause.push_back(input_var[i]);
+		vector<int> clause{-output_var,-input_var[i]};
+		clauses.push_back(clause);
 	}
 
 	clauses.push_back(sum_clause);
@@ -120,3 +70,11 @@ vector<vector<int>> nor_clause(vector<int> input_var, int output_var) {
 	return clauses;
 }
 
+vector<vector<int>> xnor2_clause(vector<int> input_var, int output_var) {
+
+	vector<int> first_clause{-output_var,input_var[0],-input_var[1]};
+	vector<int> sec_clause{-output_var,-input_var[0],input_var[1]};
+	vector<vector<int>> clauses{first_clause,sec_clause};
+
+	return clauses;
+}
